e59c7b4b69b8da0adf0304ab2a53c635a0f133b6
Tibbs <terryjw.97@gmail.com>
2024-10-14 15:29:25 +0100

    Finished ticket 3

--- db/app.js
+++ db/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { getTopics } = require("./controllers/nc-news.controllers");
 const app = express();
+const endpoints = require("../endpoints.json");
 
 app.get("/api/topics", getTopics);
 
+app.get("/api", (req, res) => {
+  res.status(200).send({ endpoints: endpoints });
+});
+
 app.all("/api/*", (req, res, next) => {
-    res.status(404).send({ msg: "Invalid input" })
-})
+  res.status(404).send({ msg: "Invalid input" });
+});
 
 module.exports = app;
