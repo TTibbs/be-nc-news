fc41f5c37abfa5461fda32b793ef87d7059420b0
Tibbs <terryjw.97@gmail.com>
2024-10-18 15:34:40 +0100

    Updated listener

--- src/listener.js
+++ src/listener.js
@@ -1,6 +1,5 @@
 const app = require("./app.js");
 
-app.listen(9090, (err) => {
-    if (err) console.log("Server didn't load", err);
-    else console.log("Listening on port 9090");
-})
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
